da78db3b8d9ce490beff5b231084963efeb216a2
wl-hsu <wlhsu.github@gmail.com>
2023-07-06 14:42:33 -0400

    add authentication

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -24,6 +26,7 @@ namespace API.Controllers
 
             return Ok(users);
         }
+
 
         [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUserById(int id)
