63c48e852f36d827f5e641478411128f4ee33a2e
wl-hsu <wlhsu.github@gmail.com>
2023-07-07 08:48:35 -0400

    init nav

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,7 +10,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
