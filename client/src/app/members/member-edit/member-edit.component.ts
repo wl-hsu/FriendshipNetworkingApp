5619907922b1778329f39a95e2ea30dcbe56b086
wl-hsu <wlhsu.github@gmail.com>
2023-07-11 10:15:14 -0400

    add edit function and warn in client site

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
@@ -11,9 +13,15 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm | undefined;
+  @HostListener('window:beforeunload', ['$event'])unloadNotification($event:any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
   member: Member | undefined;
   user: User | null = null;
-  constructor(private accountService: AccountService, private memberService: MembersService) {
+  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => this.user = user
    })
@@ -22,7 +30,7 @@ export class MemberEditComponent implements OnInit {
   ngOnInit(): void {
     this.loadMember();
   }
-  
+
   loadMember() {
     if (!this.user) return;
     this.memberService.getMember(this.user.username).subscribe({
@@ -30,4 +38,10 @@ export class MemberEditComponent implements OnInit {
     })
   }
 
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
+
+  }
 }
