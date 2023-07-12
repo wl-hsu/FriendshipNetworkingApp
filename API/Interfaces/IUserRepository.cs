f68c7531d7bb5a9a0575886886fed348018a4741
wl-hsu <wlhsu.github@gmail.com>
2023-07-12 07:20:19 -0400

    upload photo api

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -16,6 +16,5 @@ namespace API.Interfaces
         Task<AppUser> GetUserByUsernameAsync(string username);
         Task<IEnumerable<MemberDto>> GetMembersAsync();
         Task<MemberDto> GetMemberAsync(string username);
-
     }
 }
