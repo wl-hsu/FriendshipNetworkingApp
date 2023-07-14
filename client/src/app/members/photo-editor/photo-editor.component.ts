16d87f0dda6d9dd150e7ef44f68c9b9e5f024565
wl-hsu <wlhsu.github@gmail.com>
2023-07-14 14:14:44 -0400

    add set main photo function in client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -2,9 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { Photo } from 'src/app/_models/photo';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
 import { environment } from 'src/environments/environment'
+import { MembersService } from 'src/app/_services/members.service';
+
 
 @Component({
   selector: 'app-photo-editor',
@@ -19,7 +22,7 @@ export class PhotoEditorComponent implements OnInit {
   baseUrl = environment.apiUrl;
   user: User | undefined;
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private membersService: MembersService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next:user => {
         if (user) this.user = user;
@@ -34,6 +37,22 @@ export class PhotoEditorComponent implements OnInit {
   fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
 
+  }
+  
+  setMainPhoto(photo: Photo) {
+    this.membersService.setMainPhoto(photo.id).subscribe({
+      next: () => {
+        if (this.user && this.member) {
+          this.user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+          this.member.photoUrl = photo.url;
+          this.member.photos.forEach(p => {
+            if (p.isMain) p.isMain = false;
+            if (p.id === photo.id) p.isMain = true;
+          })
+        }
+      }
+    })
   }
 
   initializeUploader() {
@@ -57,4 +76,5 @@ export class PhotoEditorComponent implements OnInit {
       }
     }
   }
+
 }
