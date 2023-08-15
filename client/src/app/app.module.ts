02cfde0c454bf2e991c74e9169edfe4cac476406
wl-hsu <wlhsu.github@gmail.com>
2023-08-15 10:17:22 -0400

    add edit user function in admin panel

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -30,6 +30,9 @@ import { DatePickerComponent } from './_forms/date-picker/date-picker.component'
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 @NgModule({
   declarations: [
@@ -51,7 +54,10 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     DatePickerComponent,
     MemberMessagesComponent,
     AdminPanelComponent,
-    HasRoleDirective
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent
   ],
   imports: [
     BrowserAnimationsModule,
