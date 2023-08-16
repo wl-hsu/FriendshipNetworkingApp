22f2f82aa3e0366b1ab9c426b36ad846cffacd33
wl-hsu <wlhsu.github@gmail.com>
2023-08-16 00:17:07 -0400

    add user online status signal in client and set up singalR in server

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7169/api/'
+  apiUrl: 'https://localhost:7169/api/',
+  hubUrl: 'https://localhost:7169/hubs/'
 };
 
 /*
