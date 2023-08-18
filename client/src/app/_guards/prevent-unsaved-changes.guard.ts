a312e3cafc607b4c7b6d4a8ef03ac7a339f92ca7
wl-hsu <wlhsu.github@gmail.com>
2023-08-17 22:42:04 -0400

    add customized confirmation dispaly for edit page

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,16 +1,14 @@
-import { Injectable } from '@angular/core';
-import { CanDeactivate } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { CanDeactivate, CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PreventUnsavedChangesGuard implements CanDeactivate<MemberEditComponent> {
-  canDeactivate(component: MemberEditComponent): boolean {
-    if (component.editForm?.dirty) {
-      return confirm('Any unsaved changes will be lost');
-    }
-    return true;
+export const PreventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  const confirmService = inject(ConfirmService);
+  
+  if (component.editForm?.dirty) {
+    return confirmService.confirm();
   }
-
-}
+  
+  return true;
+};
