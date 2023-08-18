e00b1d206da925982e1d6a2185373009f74007a0
wl-hsu <wlhsu.github@gmail.com>
2023-08-18 05:21:37 -0400

    set up scroll in member message card

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -27,7 +27,10 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: () => this.router.navigateByUrl('/members'),
+      next: () => {
+        this.router.navigateByUrl('/members');
+        this.model={};
+      },
     })
   }
 
